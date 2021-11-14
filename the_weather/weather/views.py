9dd4439fcba4174673f1c32bef069ee84ea800a8
Hope Ruse <hruse@bu.edu>
2021-11-14 15:11:16 -0500

    Weather Prototype

---
+++ the_weather/weather/views.py
@@ -0,0 +1,33 @@
+import requests
+from django.shortcuts import render
+from .models import City
+from .forms import CityForm
+
+def index(request):
+    url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid=43aa2aa0c693d139521cf845bb381b3a'
+
+    if request.method == 'POST':
+        form = CityForm(request.POST)
+        form.save()
+
+    form = CityForm()
+
+    cities = City.objects.all()
+
+    weather_data = []
+
+    for city in cities:
+
+        city_weather = requests.get(url.format(city)).json() #request the API data and convert the JSON to Python data types
+
+        weather = {
+            'city' : city.name,
+            'temperature' : city_weather['main']['temp'],
+            'description' : city_weather['weather'][0]['description'],
+            'icon' : city_weather['weather'][0]['icon']
+        }
+
+        weather_data.append(weather)
+
+    context = {'weather_data' : weather_data, 'form' : form}
+    return render(request, 'weather/index.html', context)

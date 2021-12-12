525d8d830114b2d4305bb033bc810e923591842b
nimuezor <65794265+nimuezor@users.noreply.github.com>
2021-12-11 23:07:50 -0500

    updated fields

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -7,8 +7,7 @@ from .models import MovieToPlaylist
 class MovieToPlaylistSerializer(serializers.ModelSerializer): 
     class Meta:
         model = MovieToPlaylist
-        fields = ['id', 'description', 'movie_title', 'movie_genre', 
-                    'host', 'created_at'] #automatically an ID field
+        fields = '__all__' #automatically an ID field
 
 class SearchMovieSerializer(serializers.ModelSerializer):
     class Meta:

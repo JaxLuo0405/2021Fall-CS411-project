36ea417d213ac77ad9faa54bb0c1457b2e7a9cef
nimuezor <65794265+nimuezor@users.noreply.github.com>
2021-12-11 23:06:51 -0500

    Updated models to fit OMDB

--- backend/api/models.py
+++ backend/api/models.py
@@ -2,9 +2,26 @@ from django.db import models
 
 # Create your models here.
 class MovieToPlaylist(models.Model):
-    movie_title = models.CharField(max_length = 50)
-    decription = models.TextField
-    movie_genre = models.CharField(max_length = 20)
-    host = models.CharField(max_length = 50, unique = True)
-    created_at = models.DateTimeField(auto_now_add = True)
-
+    Title = models.CharField(max_length=100)
+    Year = models.CharField(max_length=100)
+    Rated = models.CharField(max_length=100)
+    Released = models.CharField(max_length=100)
+    Runtime = models.CharField(max_length=100)
+    Genre = models.CharField(max_length=100)
+    Director = models.CharField(max_length=100)
+    Writer = models.CharField(max_length=1000)
+    Actors = models.CharField(max_length=1000)
+    Plot = models.CharField(max_length=1000)
+    Language = models.CharField(max_length=100)
+    Country = models.CharField(max_length=100)
+    Awards = models.CharField(max_length=100)
+    Poster = models.CharField(max_length=1000)
+    Metascore = models.CharField(max_length=100)
+    imdbRating = models.CharField(max_length=100)
+    imdbVotes = models.CharField(max_length=100)
+    imdbID = models.CharField(max_length=100)
+    Type = models.CharField(max_length=100)
+    DVD = models.CharField(max_length=100)
+    BoxOffice = models.CharField(max_length=100)
+    Production = models.CharField(max_length=100)
+    Website = models.CharField(max_length=100)

7eb91b9b4826a398e01091111c9cdc3e6d1cde6e
nimuezor <65794265+nimuezor@users.noreply.github.com>
2021-12-16 13:53:40 -0800

    Added url with Search view

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -4,5 +4,6 @@ from .views import MovieToPlaylistView, SearchMovieView, Generate
 urlpatterns = [
     path('movie', MovieToPlaylistView.as_view()),
     path('search-movie', SearchMovieView.as_view()),
-    path('generate', Generate.as_view())
+    path('generate', Generate.as_view()),
+    path('find-movie', Search.as_view()),
 ]
